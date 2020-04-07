b23eb2798ac7f3d1ef76bacfc6d0dddaeb609bde
Adam Spencer <aspencer0919@gmail.com>
2020-04-07 03:29:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,57 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  return holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash.each do |season, data|
+    if season == :winter
+      data.each do |holiday, key|
+        key << supply
+      end
+    end
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash.each do |season, data|
+    if season == :spring
+      data.each do |holiday, key|
+        key << supply
+      end
+    end
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash.each do |k, v|
+    if k == season
+      v[holiday_name] = supply_array
+    end
+  end
+  return holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  return_array = []
+  holiday_hash.each do |k, v|
+    if k == :winter
+      v.each do |k1, v1|
+        return_array << v1
+      end
+    end
+  end
+  return_array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -59,12 +85,14 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  bbq_holidays = []
+  holiday_hash.each do |k, v|
+    if k == :winter || :spring || :summer || :fall
+    k.each do |k1, v1|
+      if v1 == "BBQ"
+        bbq_holidays << k1
+      end
+    end
+  end
+  bbq_holidays.flatten
 end
-
-
-
-
-
-
-
